d709fc255ea002c4ea50899a206f769139e72024
ToshihitoKon <toshihito.kon@gmail.com>
2019-12-29 22:54:01 +0900

    パラノイアモードで爆速

--- cue_flac/queue/makeflaccue.sh
+++ cue_flac/queue/makeflaccue.sh
@@ -2,7 +2,7 @@
 
 mkdir ${1}
 cd ${1}
-cdrdao read-cd --rspeed 48 --with-cddb --cddb-servers "freedbtest.dyndns.org:/~cddb/cddbutf8.cgi" --datafile ${1}.bin ${1}.toc
+cdrdao read-cd --rspeed 50 --paranoia-mode 0 --with-cddb --cddb-servers "freedbtest.dyndns.org:/~cddb/cddbutf8.cgi" --datafile ${1}.bin ${1}.toc
 cueconvert -i toc -o cue ${1}.toc ${1}.tmp
 printf "$(cat ${1}.tmp)" | sed -e "s/\.bin/\.flac/g" > ${1}.cue
 echo "" >> ${1}.cue
@@ -12,4 +12,4 @@ rm ${1}.{bin,wav,tmp}
 
 # cue file auto complete
 sed -i -f ../freedb.sed ${1}.cue
-sed -i -f ../performer.sed ${1}.cue
+#sed -i -f ../performer.sed ${1}.cue
